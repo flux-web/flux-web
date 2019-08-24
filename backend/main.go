715f24f16a5956ecc2575b747205d54af6268879
Yohanan Baruchel <ybaruchel@gmail.com>
2019-08-24 23:35:15 +0300

    Restore namespaces

--- backend/main.go
+++ backend/main.go
@@ -8,9 +8,8 @@ import (
 
 func main() {
 	beego.Router("/api/v1", &controllers.WorkloadController{}, "get:ListWorkloads")
-	//beego.Router("/namespaces", &controllers.WorkloadController{}, "get:ListServices")
-	//beego.Router("/workloads/:ns", &controllers.WorkloadController{}, "get:ListWorkloads")
-	//beego.Router("/release", &controllers.WorkloadController{}, "post:ReleaseWorkloads")
+	beego.Router("/workloads/:ns", &controllers.WorkloadController{}, "get:ListWorkloads")
+	beego.Router("/release", &controllers.WorkloadController{}, "post:ReleaseWorkloads")
 
 	beego.Router("/ws/v1", &controllers.WebSocketController{}, "get:ReleaseWorkloads")
 
