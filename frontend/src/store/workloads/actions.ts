77b8410e96fc09e974bf00452c93403cdbbafe15
Yohanan Baruchel <ybaruchel@gmail.com>
2019-08-26 14:34:11 +0300

    wip

--- frontend/src/store/workloads/actions.ts
+++ frontend/src/store/workloads/actions.ts
@@ -10,7 +10,7 @@ export const actions: ActionTree<WorkloadsState, RootState> = {
     changeSearchTerm: ({commit}, searchTerm: string) => commit('CHANGE_SEARCH_TERM', searchTerm),
     updateWorkloadStatus:  ({commit}, payload) => commit('UPDATE_WORKLOAD_STATUS', payload),
     fetchWorkloads: ({commit}, namespace: string): any => axios.get('/workloads/' + namespace).then(
-        (response) => {
+        (response: any)=> {
             const workloads = workloadsTransformer(response.data);
             commit('UPDATE_WORKLOADS', workloads);
         },
