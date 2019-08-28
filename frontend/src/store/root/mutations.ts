dd3d2049f67a52d0141f1b1069332778622bb9e9
Yohanan Baruchel <ybaruchel@gmail.com>
2019-08-28 21:34:02 +0300

    Redirect ws directly to backend

--- frontend/src/store/root/mutations.ts
+++ frontend/src/store/root/mutations.ts
@@ -9,7 +9,10 @@ export const mutations: MutationTree<RootState> = {
     },
     SOCKET_ONCLOSE: (state: RootState, event: any) => state.socket.isConnected = false,
     SOCKET_ONERROR: (state: RootState, event: any) => console.error(state, event),
-    SOCKET_ONMESSAGE: (state: RootState, message: string) => state.socket.message = message,
+    SOCKET_ONMESSAGE: (state: RootState, message: string) => {
+        console.log(message);
+        state.socket.message = message;
+    },
     SOCKET_RECONNECT: (state: RootState, count: number) => console.info(state, count),
     SOCKET_RECONNECT_ERROR: (state: RootState) => state.socket.reconnectError = true,
 };
