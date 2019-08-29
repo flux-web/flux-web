bb27d718c3891b83019cfd003a2ec2292bcbfbb3
Yohanan Baruchel <ybaruchel@gmail.com>
2019-08-29 13:49:17 +0300

    Reformat tag response from ws update

--- frontend/src/store/root/mutations.ts
+++ frontend/src/store/root/mutations.ts
@@ -17,7 +17,7 @@ export const mutations: MutationTree<RootState> = {
             id: message.Workload,
             container: message.Container,
         },
-        tag: message.Tag
+        tag: message.Tag.split(':').pop(),
     }),
     SOCKET_RECONNECT: (state: RootState, count: number) => console.info(state, count),
     SOCKET_RECONNECT_ERROR: (state: RootState) => state.socket.reconnectError = true,
