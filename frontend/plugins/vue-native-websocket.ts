a67bd603666648568675bb8a6f07dc0ef1a21e44
Yohanan Baruchel <ybaruchel@gmail.com>
2019-08-30 11:33:41 +0300

    Add logs to ws

--- frontend/plugins/vue-native-websocket.ts
+++ frontend/plugins/vue-native-websocket.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import VueNativeSock from 'vue-native-websocket';
-import store from '../store/index';
 import config from '../config';
 
-Vue.use(VueNativeSock, 'ws://' + config.wsUrl, {
-  ...store,
-  reconnection: true,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 3000,
-  format: 'json',
-});
+export default ({ store }: { store: any }) => {
+  Vue.use(VueNativeSock, 'ws://' + config.wsUrl, {
+    store,
+    reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 3000,
+    format: 'json',
+  });
+}
+
