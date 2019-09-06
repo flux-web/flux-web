de75d66695e9e6d2a5ede81dd46628566b4c3677
Ido braunstain <idob@172-10-71-30.lightspeed.jcsnms.sbcglobal.net>
2019-09-06 08:59:09 +0300

    readonly for backend routes

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"fmt"
+	"strconv"
 	"flux-web/controllers"
 
 	"github.com/astaxie/beego"
@@ -12,16 +15,30 @@ func main() {
 	h := controllers.InitHub()
 	go h.Run()
 
+	releaseNs := beego.NewNamespace("/api",
+		beego.NSNamespace("/rel/v1",
+			beego.NSCond(func(ctx *context.Context) bool {
+				if readOnly, err := strconv.ParseBool(os.Getenv("READ_ONLY")); err != nil  {
+					return true
+				} else{
+					return !readOnly
+				}
+			}),
+			beego.NSRouter("/release", &controllers.WorkloadController{}, "post:ReleaseWorkloads"),
+		),
+	)
+
 	apiNs := beego.NewNamespace("/api",
 		beego.NSNamespace("/v1",
 			beego.NSRouter("/namespaces", &controllers.NamespaceController{}, "get:ListNamespaces"),
 			beego.NSRouter("/workloads/:ns", &controllers.WorkloadController{}, "get:ListWorkloads"),
-			beego.NSRouter("/release", &controllers.WorkloadController{}, "post:ReleaseWorkloads"),
 			beego.NSGet("/health", func(ctx *context.Context) {
             	ctx.Output.Body([]byte("All good"))
         	}),
 		),
 	)
+
+	beego.AddNamespace(releaseNs)
 	beego.AddNamespace(apiNs)
 
 	beego.Router("/ws/v1", &controllers.WebSocketController{}, "get:ReleaseWorkloads")
