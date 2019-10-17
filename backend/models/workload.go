87d1ced56a82fef0d6284deab35b76b28fb58fd8
Ido braunstain <idob@172-10-71-30.lightspeed.jcsnms.sbcglobal.net>
2019-10-17 22:28:41 +0300

    filter tags

--- backend/models/workload.go
+++ backend/models/workload.go
@@ -2,6 +2,9 @@ package models
 
 import (
 	"encoding/json"
+	"fmt"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/astaxie/beego/logs"
@@ -27,34 +30,38 @@ type Workload struct {
 	Type       string
 	Automated  bool
 	Policies   interface{}
-	Containers []struct {
-		Name    string `json:"Name"`
-		Status  string
-		Current struct {
-			ID      string `json:"ID"`
-			Digest  string `json:"Digest"`
-			ImageID string `json:"ImageID"`
-			Labels  struct {
-				OrgLabelSchemaBuildDate       time.Time `json:"org.label-schema.build-date"`
-				OrgOpencontainersImageCreated time.Time `json:"org.opencontainers.image.created"`
-			} `json:"Labels"`
-			CreatedAt   time.Time `json:"CreatedAt"`
-			LastFetched time.Time `json:"LastFetched"`
-		} `json:"Current"`
-		Available []struct {
-			ID      string `json:"ID"`
-			Digest  string `json:"Digest"`
-			ImageID string `json:"ImageID"`
-			Labels  struct {
-				OrgLabelSchemaBuildDate       time.Time `json:"org.label-schema.build-date"`
-				OrgOpencontainersImageCreated time.Time `json:"org.opencontainers.image.created"`
-			} `json:"Labels,omitempty"`
-			CreatedAt   time.Time `json:"CreatedAt"`
-			LastFetched time.Time `json:"LastFetched"`
-		} `json:"Available"`
-		AvailableImagesCount int `json:"AvailableImagesCount"`
-		FilteredImagesCount  int `json:"FilteredImagesCount"`
-	} `json:"Containers"`
+	Containers []Container
+}
+
+type Container struct {
+	Name    string `json:"Name"`
+	Status  string
+	Current struct {
+		ID      string `json:"ID"`
+		Digest  string `json:"Digest"`
+		ImageID string `json:"ImageID"`
+		Labels  struct {
+			OrgLabelSchemaBuildDate       time.Time `json:"org.label-schema.build-date"`
+			OrgOpencontainersImageCreated time.Time `json:"org.opencontainers.image.created"`
+		} `json:"Labels"`
+		CreatedAt   time.Time `json:"CreatedAt"`
+		LastFetched time.Time `json:"LastFetched"`
+	} `json:"Current"`
+	Available            []Available
+	AvailableImagesCount int `json:"AvailableImagesCount"`
+	FilteredImagesCount  int `json:"FilteredImagesCount"`
+}
+
+type Available struct {
+	ID      string `json:"ID"`
+	Digest  string `json:"Digest"`
+	ImageID string `json:"ImageID"`
+	Labels  struct {
+		OrgLabelSchemaBuildDate       time.Time `json:"org.label-schema.build-date"`
+		OrgOpencontainersImageCreated time.Time `json:"org.opencontainers.image.created"`
+	} `json:"Labels,omitempty"`
+	CreatedAt   time.Time `json:"CreatedAt"`
+	LastFetched time.Time `json:"LastFetched"`
 }
 
 func (w Workload) getWorkloadKey(container string) string {
@@ -79,10 +86,15 @@ func initWorkloads(workloads []Workload, services Dictionary) []Workload {
 			workloads[i].Type = helmreleaseType
 		}
 
-		workloads[i].Policies = services[workload.ID].Policies
 		workloads[i].Automated = services[workload.ID].Automated
 
 		for j, container := range workload.Containers {
+
+			if services[workload.ID].Policies != nil {
+				workloads[i].Policies = services[workload.ID].Policies
+				workloads[i].Containers[j].Available = filterOutTags(services[workload.ID].Policies, workloads[i].Containers[j].Available, workloads[i].Containers[j].Name)
+			}
+
 			workloadStatus := MemGet(workload.getWorkloadKey(container.Name))
 			if workloadStatus != "" {
 				workloads[i].Containers[j].Status = workloadStatus
@@ -98,10 +110,39 @@ func initWorkloads(workloads []Workload, services Dictionary) []Workload {
 	return workloads
 }
 
+func filterOutTags(policies interface{}, available []Available, containerName string) []Available {
+	policiesMap := policies.(map[string]interface{})
+	l.Println(containerName)
+	for k, v := range policiesMap {
+		availableName := strings.Split(k, ".")
+		if len(availableName) == 1 {
+			continue
+		}
+		tagPattern := strings.Split(fmt.Sprintf("%s", v), ":")
+		if tagPattern[0] != "glob" {
+			continue
+		}
+		l.Println("tagFilter: " + tagPattern[1])
+		if containerName == availableName[1] {
+			for _, ava := range available {
+				re := regexp.MustCompile(tagPattern[1])
+
+				if !re.MatchString(`*`) {
+					l.Println("need to delete " + strings.Split(ava.ID, ":")[1])
+				}
+			}
+		}
+
+	}
+
+	return available
+}
+
 func filterOutWorkloads(services Dictionary) Dictionary {
 	for k, _ := range services {
 		if services[k].Antecedent != "" || services[k].ReadOnly == NotInRepo {
 			delete(services, k)
+			continue
 		}
 	}
 	return services
