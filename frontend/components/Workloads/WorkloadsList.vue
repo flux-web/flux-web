78cda4d7f3861249d36b28ce6fa1d2778d426743
derkinderfietsen <20744388+derkinderfietsen@users.noreply.github.com>
2020-09-23 20:09:20 +0300

    Fix/ui fixes (#86)
    * change empty workloads table text
    
    * add multiselect plugin to namespaces select
    
    * fix no workloads error on transformer

--- frontend/components/Workloads/WorkloadsList.vue
+++ frontend/components/Workloads/WorkloadsList.vue
@@ -110,6 +110,15 @@ export default class WorkloadsList extends Vue {
 <style lang="scss">
 @import "../../assets/scss/include";
 
+/* Hack to replace empty table text */
+.vgt-text-disabled {
+  visibility: hidden;
+}
+.vgt-text-disabled:after {
+  content:'No workloads found in namespace.'; 
+  visibility: visible;
+}
+
 .workloads-list {
   height: calc(100% - 110px);
   min-height: 250px;
